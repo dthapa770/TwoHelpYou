c2c845017b18b56516ab41d87a9088dca5f8d9ea
Justin Lam <jlam18@mail.sfsu.edu>
2021-09-20 15:39:33 -0700

    Removed hbs and installed nodemon as part of package. Renamed layout.hbs to home.hbs and moved it into created folder called layouts, followed by adding partials folder into views folder. Changed app.js to require express-handlebars and inputted app engine to use handlebars.

--- application/server/app.js
+++ application/server/app.js
@@ -4,10 +4,22 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var handlebars = require("express-handlebars");
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+app.engine(
+  "hbs",
+  handlebars({
+    layoutsDir: path.join(__dirname, "views/layouts"),
+    partialsDir: path.join(__dirname, "views/partials"),
+    extname: ".hbs",
+    defaultLayout: "home",
+    helpers: {}
+  })
+)
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
