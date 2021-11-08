79ba0ad710bd07eaff33665be3cc8d0dd03aceb2
Justin Lam <jlam18@mail.sfsu.edu>
2021-11-07 17:45:17 -0800

    Format routes\users.js and prep register.hbs for file upload

--- application/server/routes/users.js
+++ application/server/routes/users.js
@@ -15,79 +15,61 @@
 
 var express = require('express');
 var router = express.Router();
-
-const UserError =require("../helpers/error/user_error");
-const {SuccessPrint,ErrorPrint}=require("../helpers/debug/debug_printers");
-
-var db = require('../config/database');
+var UserModel = require('../models/user_model');
+const { SuccessPrint, ErrorPrint } = require('../helpers/debug/debug_printers');
+const UserError = require('../helpers/error/user_error');
 
 //var crypto=require('crypto');
-var UserModel = require('../models/user_model');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+	res.send('respond with a resource');
 });
 
 /**
  * User registration route.
  */
-router.post('/register',(req,res,next) =>{
-  var first_name = req.body.first_name;
-  var last_name = req.body.last_name;
-  var username = req.body.username;
-  var email = req.body.email;
-  var password = req.body.password;
-  console.log(password);
+router.post('/register', (req, res, next) => {
+	var first_name = req.body.first_name;
+	var last_name = req.body.last_name;
+	var username = req.body.username;
+	var email = req.body.email;
+	var password = req.body.password;
 
-     UserModel.UsernameExists(username)
-      .then((userDoesNameExists) =>{
-          if(userDoesNameExists){
-              throw new UserError(
-                  "Registration Failed:Username already exist!!",
-                  "/registration",
-                  200
-              );
-          } else {
-              return UserModel.EmailExists(email);
-          }
-  })
-  .then((emailDoesExists) => {
-      if(emailDoesExists){
-          throw new UserError(
-              "Registration Failed:Email already exist!!",
-              "/registration",
-              200
-          );
-      }else{
-          return UserModel.Create(first_name,last_name,username, password, email);
-      }
-  })
-  .then((createUserId) => {
-      if (createUserId < 0) {
-          throw new UserError(
-              "Server Error: User cannot be created",
-              "/registration",
-              500
-          );
-      } else {
-          SuccessPrint("Users.js -->User was created!!");
-          res.redirect('/login');
-      }
-  })
-  .catch((err) => {
-          ErrorPrint("user cannot be made", err);
-          if (err instanceof UserError) {
-              ErrorPrint(err.GetMessage());
-              //req.flash('error', err.GetMessage());
-              res.status(err.GetStatus());
-              res.redirect(err.GetRedirectURL());
-          } else {
-              next(err);
-          }
-      });
-  });
-
-
+	UserModel.UsernameExists(username)
+		.then((user_name_exists) => {
+			if (user_name_exists) {
+				throw new UserError('Registration Failed:Username already exist!!', '/registration', 200);
+			} else {
+				return UserModel.EmailExists(email);
+			}
+		})
+		.then((email_exists) => {
+			if (email_exists) {
+				throw new UserError('Registration Failed:Email already exist!!', '/registration', 200);
+			} else {
+				return UserModel.Create(first_name, last_name, username, password, email, image_name);
+			}
+		})
+		.then((create_user) => {
+			if (create_user < 0) {
+				throw new UserError('Server Error: User cannot be created', '/registration', 500);
+			} else {
+				SuccessPrint('Users.js -->User was created!!');
+				res.redirect('/login');
+			}
+		})
+		.catch((err) => {
+			ErrorPrint('user cannot be made', err);
+			if (err instanceof UserError) {
+				ErrorPrint(err.GetMessage());
+				//req.flash('error', err.GetMessage());
+				res.status(err.GetStatus());
+				res.redirect(err.GetRedirectURL());
+			} else {
+				next(err);
+			}
+		});
+});
 
 module.exports = router;
