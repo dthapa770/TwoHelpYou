b860f385dabb651ec645cc0e2a7b5c543dff0e7f
Wesley Xu <wxu3@mail.sfsu.edu>
2021-11-15 23:53:37 -0800

    Frontend Validation and UI Improvements

--- application/client/public/js/registration.js
+++ application/client/public/js/registration.js
@@ -86,6 +86,16 @@ function ValidateEmail(email_to_add){
  * @param password 
  * @returns 
  */
+ const togglePassword = document.querySelector('#togglePassword');
+ const password = document.querySelector('#password');
+
+ togglePassword.addEventListener('click', function (e) {
+    // toggle the type attribute
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+    // toggle the eye / eye slash icon
+    this.classList.toggle('bi-eye');
+});
 function ValidatePassword(password){
     var message = 'Password does not contain:'
     
@@ -184,3 +194,22 @@ register_form.onsubmit = function(event){
     }
     event.preventDefault();
 }
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+    'use strict';
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation');
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms).forEach((form) => {
+      form.addEventListener('submit', (event) => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  })();
