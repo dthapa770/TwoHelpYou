834ca085ccc3d535e4bcfede6eb922d8d6ed42cf
Justin Lam <jlam18@mail.sfsu.edu>
2021-12-04 14:59:03 -0800

    Changes to dashboard using w3school for side panel.

---
+++ application/client/public/js/dashboard.js
@@ -0,0 +1,24 @@
+/******************************************************************************
+ * Class: CSC 0648-03 Software Engineering Fall 2021
+ * Team: 1
+ * Name:  Justin Lam
+ *        Aviral Puri
+ *        Dinesh Thapa
+ *        Kurt D Resayo
+ *        Wesley J Xu
+ *        Chung Hei Fong
+ * 
+ * File: dashboard.js
+ * 
+ * Description: Dashboard Functionailty provided by w3school.
+ *****************************************************************************/
+
+/* Set the width of the sidebar to 250px (show it) */
+function openNav() {
+	document.getElementById('side_panel').style.width = '250px';
+}
+
+/* Set the width of the sidebar to 0 (hide it) */
+function closeNav() {
+	document.getElementById('side_panel').style.width = '0';
+}
